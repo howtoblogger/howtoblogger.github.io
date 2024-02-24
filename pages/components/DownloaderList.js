863f1664a5f8536eb6763c7765612c77295f734c
Jayasurya Mailsamy <67519756+oksurya@users.noreply.github.com>
2024-02-24 19:07:20 +0530

    dfgfg

--- pages/components/DownloaderList.js
+++ pages/components/DownloaderList.js
@@ -38,8 +38,8 @@ export default function DownloaderList() {
     <p>Save Instagram Videos quickly and easily with our efficient downloader tool.</p>
     <div class="card-actions justify-center">
     <Link className='btn btn-block btn-primary' href="/video">
-      <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" aria-hidden="true" height="24" width="24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
-       Download Photos  </Link>
+    <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="24" width="24" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0z"></path><path d="M4 6H2v14c0 1.1.9 2 2 2h14v-2H4V6zm16-4H8c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-8 12.5v-9l6 4.5-6 4.5z"></path></svg>
+       Download Videos  </Link>
     </div>
   </div>
 </div>
